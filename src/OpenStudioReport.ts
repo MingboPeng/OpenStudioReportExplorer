f4c1ae4291b1673be6a5db2d80d38965221a76c0
Mingbo Peng <mingo1214@gmail.com>
2018-08-02 21:44:23 -0400

    gets EUI, floorArea

--- src/OpenStudioReport.ts
+++ src/OpenStudioReport.ts
@@ -21,20 +21,80 @@ export class OpenStudioReport{
 
     static GetData(checkedUrl:string):OpsReportData
     {
-        let reportData:OpsReportData = {EUI:0,TotalFloorArea:0};
+        var url:string = checkedUrl;
+        var doc:Document = new Document();
+        let reportData = new OpsReportData();
+
+        d3.html(url)
+        .then(
+            function (d:Document) {
+                console.log('Loading HTML file');
+                doc = d;
+            }
+        ).then(
+            function () {
+                console.log('Extracting data from HTML');
+                let eui = OpenStudioReport.GetEUI(doc,reportData);
+                    
+                
+                console.log(eui);
+
+
+            }
+            //let sel = new d3.selection(doc,);
+            
+            
+            // var htmlObj = c._groups[0];
+            
+            // alert( "Load htmlFileUrl2 was performed." );
+            // var row = htmlObj[4];
+            // var textContent = row.cells[1].innerHTML;
+            // var textUnit = row.cells[2].innerHTML;
+            // alert( textContent);
+        )
+
+
+        
         //get data from html report
-        reportData.EUI = 1;
-        reportData.TotalFloorArea = 2;
 
         return reportData;
+
+    }
+
+    static GetEUI(document:Document,reportObj:OpsReportData):OpsReportData {
+
+        let c = d3.select(document).selectAll("#table_0 tbody tr").nodes()
+        .map(function (d) {
+            return <HTMLTableRowElement>d;
+        })
+        // c.forEach(element => {
+            
+        // });
+        // c.forEach(element => {
+        //     let rowEle = <HTMLTableRowElement> element;
+            
+        //     rowEle.cells.length
+        //     console.log(rowEle.cells.item(1).innerHTML);
+        // })
+
+        
+        let eui:number = Number(c[4].cells.item(1).innerHTML);
+        let area:number = Number(c[3].cells.item(1).innerHTML.replace(',',''));
+        let euiUnit: string = c[4].cells.item(2).innerHTML;
+        
+        reportObj.EUI = eui;
+        reportObj.TotalFloorArea = area;
+        reportObj.EUIUnit = euiUnit;
+        return reportObj;
 
     }
 
 
 }
 
-interface OpsReportData {
-    EUI:number;
-    TotalFloorArea:number;
+class OpsReportData {
+    EUI:number = 0;
+    EUIUnit:string = '';
+    TotalFloorArea:number = 0;
     //etc
 }
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
+import { OpenStudioReport } from "./OpenStudioReport";
+
+
+var url = 'https://gist.githubusercontent.com/MingboPeng/e68e488045cbc74d9cbeb41ee87724f7/raw/openstudio_results_copy_report.html';
+
+OpenStudioReport.GetData(url);
+
 function greeter(person:string) {
     return "Hello, " + person;
 }
